670bc630c8bce014c361b070789b8d26e5ba7efd
maseh87 <maseh87@gmail.com>
2014-01-14 01:18:06 -0800

    added mongo database

---
+++ server.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var db = mongoose.connect('mongodb://localhost/calendar');
+app.use(express.static(__dirname + '/app'));
+
+app.get('*', function(req, res) {
+  res.sendfile('app/index.html');
+})
+
+
+
+
+app.listen(3000);
+console.log('port is listening 3k');
